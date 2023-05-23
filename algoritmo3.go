d682cb23010a50d7e7dafd1a016f5526cbcf464c
Hu0no <126472671+Hu0no@users.noreply.github.com>
2023-05-23 09:23:43 -0300

    Create algoritmo3.go

---
+++ algoritmo3.go
@@ -0,0 +1,10 @@
+package main
+
+func ReverseString(ptr *string) {
+	runes := []rune(*ptr)
+	length := len(runes)
+
+	for i, j := 0, length-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+}
